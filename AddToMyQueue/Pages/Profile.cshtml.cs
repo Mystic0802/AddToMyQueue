0a484428e8c5c1a1c9fccd6e2db6eec1b6f53d91
Nathan Webb <nathanwebb02@outlook.com>
2022-08-26 05:30:22 +0100

    Base brofile page #4

--- AddToMyQueue/Pages/Profile.cshtml.cs
+++ AddToMyQueue/Pages/Profile.cshtml.cs
@@ -1,6 +1,9 @@
 using AddToMyQueue.Data;
+using AddToMyQueue.Data.Models;
+using AddToMyQueue.Data.Models.Spotify;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection.Metadata.Ecma335;
 
 namespace AddToMyQueue.Web.Pages
 {
@@ -17,9 +20,63 @@ namespace AddToMyQueue.Web.Pages
 
         public void OnGet(string userId)
         {
-            //var userProfile = _context.Users
+            // if no userID is entered in the URL, show users own profile
+            if (userId == null || userId == string.Empty)
+            {
+                // Check if logged in, use their userId if so. ask to sign in/up if not
+                userId = TryGetCurrentUserId() ?? "";
 
-            // if no profile is found, show "profile not found" stuff
+                if (userId == "")
+                {
+                    Response.Redirect("/login");
+                    return;
+                }
+                
+
+            }
+            
+            var userProfile = _context.Users.Where(u => u.UserId == userId).FirstOrDefault();
+
+            if (userProfile == null)
+            {
+                // if no profile is found, show "profile not found" stuff
+                return;
+            }
+
+            Username = userProfile.Username;
+
+            DisplayProfileInfo(userProfile);
+
+            var userSpotifyAccountId = _context.UserSpotifyAccounts.Where(a => a.UserId == userId).FirstOrDefault()?.SpotifyId;
+            var userSpotifyAccount = _context.SpotifyAccounts.Where(a => a.SpotifyId == userSpotifyAccountId).FirstOrDefault();
+
+            if (userSpotifyAccount == null)
+            {
+                // if not spotify user found, show spotify auth stuff
+                return;
+            }
+
+            DisplaySpotifyAccount(userSpotifyAccount);
+        }
+
+        private string? TryGetCurrentUserId()
+        {
+            return null;
+        }
+
+        private void DisplayProfileInfo(User userProfile)
+        {
+            
+        }
+
+        private void DisplaySpotifyAccount(SpotifyAccount spotifyAccount)
+        { 
+            
+        }
+
+        public void StartSpotifyAuthFlor()
+        {
+
         }
     }
 }
